0887bef5b7578a7b00f42f574bd389454a8ab4f8
Santi-Villagran <141844073+Santi-Villagran@users.noreply.github.com>
2023-08-09 11:18:14 -0300

    agregado

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -6,6 +6,9 @@ public class PruebaGit {
     public static void main(String[] args) {
         
         System.out.println("Hola");
+        for(int i=1; i<4; i++){
+            System.out.println("prueba: "+i);
+        }
         
     }
     
